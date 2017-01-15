a9d25fb10b84713d9b16402b6ea3ee752b5350d4
lene marlin <ybs18s@163.com>
2017-01-15 23:23:37 +0800

    add login and signup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "早安!这是多么美好的一天"
+  end
 end
--- app/helpers/flashes-helper.rb
+++
@@ -1,9 +0,0 @@
-medule FlashHelper
-  FLASH_CLASSES = {alert: 'danger', notice: 'success', warn: 'warning'}
-  def flash_class
-    FLASH_CLASSES.fetch.key.to_sym, key
-
-  def user_facing_flashes
-    flash.to_hash.slice "alert", "notice", "warning"
-  end
-end
